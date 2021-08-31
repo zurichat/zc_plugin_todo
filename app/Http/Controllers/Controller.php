f45b3f0fc5e7c97924aa544caeb3020263ac56a4
andrew21-mch <nfonandrew73@gmail.com>
2021-08-31 07:12:57 +0100

    created and routed the editTask todo api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,8 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    function editTask(){
+      return "Redis is such a torn";
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get("/tasks", [Controller::class, 'editTask']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
