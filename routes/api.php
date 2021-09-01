7a88b2d376e051d35a77da2304288e183da08b9a
Olayinka Promise <olayinkapromise80@gmail.com>
2021-09-01 14:12:55 +0100

    added task controller

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\PluginInfoController;
-use App\Http\Controllers\TaskDemoController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
