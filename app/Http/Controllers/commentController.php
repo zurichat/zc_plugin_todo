4f74a9a10c08da84b5dc26a447b6085bc0e4de15
unknown <alabo2015@yahoo.com>
2021-09-01 17:13:22 +0100

    comment controller class

---
+++ app/Http/Controllers/commentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\TaskService;
+use Illuminate\Http\Request;
+
+class commentController extends Controller
+{
+
+    public function __construct(){}
+
+
+
+    public function update($cid)
+    {
+ 
+    }
+
+    public function delete($cid)
+    {
+
+    }
+}
