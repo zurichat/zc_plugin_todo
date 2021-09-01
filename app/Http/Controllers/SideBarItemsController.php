c9491f6bb462f4722e5ef659dc06cd563f231f94
Ericogia <ogia.aghahowa@gmail.com>
2021-09-01 12:25:16 +0100

    Ping Url Added

---
+++ app/Http/Controllers/SideBarItemsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SideBarItemsController extends Controller
+{
+    public function serveMenuItems()
+    {
+
+        $sideBarItems = [
+            'item_1' => 'Task Leaf',
+            'item_2' => 'Schedules',
+            'item_3' => 'Calendar'
+        ];
+
+        return response()->json([
+            'status' => 'success',
+            'type' => 'sidebar items',
+            'items' => $sideBarItems
+        ], 200);
+    }
+}
