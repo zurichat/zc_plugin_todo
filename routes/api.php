edc3a3ffae3c278e0bf3841c897b0b355031760c
promisszn <82944796+promisszn@users.noreply.github.com>
2021-09-01 08:21:23 +0100

    added task demo controller

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\PluginInfoController;
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskDemoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
