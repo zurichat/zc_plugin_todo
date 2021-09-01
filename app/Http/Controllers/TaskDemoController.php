a634cc3464feee0fd5ade13d3edf261b77be4e9c
OfficialOzioma <dikep15@gmail.com>
2021-09-01 22:55:24 +0100

    update

--- app/Http/Controllers/TaskDemoController.php
+++ app/Http/Controllers/TaskDemoController.php
@@ -5,37 +5,31 @@ namespace App\Http\Controllers;
 use App\Services\TaskService;
 use Illuminate\Http\Request;
 
-class TaskDemoController extends Controller
-{
+class TaskDemoController extends Controller {
     protected $taskService;
 
-    public function __construct(TaskService $taskService)
-    {
+    public function __construct( TaskService $taskService ) {
         $this->taskService = $taskService;
     }
 
-    public function index()
-    {
-        return response()->json($this->taskService->all());
+    public function index() {
+        return response()->json( $this->taskService->all() );
     }
 
-    public function store(Request $request)
-    {
-        return response()->json($this->taskService->create($request->all()));
+    public function store( Request $request ) {
+        return response()->json( $this->taskService->create( $request->all() ) );
     }
 
-    public function show($id)
-    {
-        return response()->json($this->taskService->find($id));
+    public function show( $id ) {
+        return response()->json( $this->taskService->find( $id ) );
     }
 
-    public function update(Request $request, $id)
-    {
-        return response()->json($this->taskService->update($request->all(), $id));
+    public function update( Request $request, $id ) {
+        return response()->json( $this->taskService->update( $request->all(), $id ) );
     }
 
-    public function delete($id)
-    {
-        return response()->json($this->taskService->delete($id));
+    public function delete( $id ) {
+        return response()->json( $this->taskService->delete( $id ) );
     }
+
 }
