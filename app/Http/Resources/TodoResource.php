d2ece0bd922fe95460a8fc9d449d7783aecf06ce
trojan782 <davidjacobs522@gmail.com>
2021-09-02 21:22:08 +0100

    formatted the data from the resource

--- app/Http/Resources/TodoResource.php
+++ app/Http/Resources/TodoResource.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class TodoResource extends JsonResource
 {
+    protected $taskService;
     /**
      * Transform the resource into an array.
      *
@@ -14,6 +15,23 @@ class TodoResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'status_id' => $this->status_id,
+            'start_Date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'parent_id' => $this->parent_id,
+            'workspace_id' => $this->workspace_id,
+            'category_id' => $this->category_id,
+            'priority_id' => $this->priority_id,
+            'recurring' => $this->recurring,
+            'reminder' =>$this->reminder,
+            'color_code' => $this->color_code,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+        // return parent::toArray($request);
     }
 }
