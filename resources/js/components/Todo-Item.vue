82e81414caa35a15eee886ce25fdffb2476274f3
[Kemmie001] <[Oluwakemisola757@gmail.com]>
2021-09-02 16:04:24 +0100

    feature: Implemented task card design

--- resources/js/components/Todo-Item.vue
+++ resources/js/components/Todo-Item.vue
@@ -2,12 +2,12 @@
     <div class="w-4/5 mx-auto todo_item border sm:w-full m-2 rounded-md bg-white "></div>
 </template>
 <script>
-export default {
-    name: "Todo-Item"
-}
+    export default {
+        name: "Todo-Item"
+    }
 </script>
 <style lang="scss" scoped>
-.todo_item {
-    min-height: 10rem;
-}
+    .todo_item {
+        min-height: 10rem;
+    }
 </style>
--- resources/js/pages/Main.vue
+++ resources/js/pages/Main.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
         <h3>
-            This is where we will be workinng
+            This is where we will be working
         </h3>
+        <taskCard title="Task title" date="Tomorrow" description="Description" />
     </div>
 </template>
 <script>
-export default {
-    name: 'Main'
-}
+    import taskCard from '../components/taskCard.vue'
+    export default {
+        name: 'Main',
+        components: {
+            taskCard
+        }
+    }
 </script>
