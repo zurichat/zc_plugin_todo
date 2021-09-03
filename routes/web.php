ec69e89f6f70997135b47edb0ca66c41f5cdeaf5
princessvincent <pricavincent2018@gmail.com>
2021-09-03 13:09:32 -0700

    this commit close #201

--- routes/web.php
+++ routes/web.php
@@ -22,17 +22,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//taskcommentcontroller
+// Route::delete('/delete', [TaskCommentController::class,'delete']);
+
 Route::get('/plugin-info', [PluginInfoController::class, 'servePluginInfo']);
 Route::get('/create-todo', [TodoController::class, 'showPage'])->name('show.create-todo');
 Route::post('/create-todo', [TodoController::class, 'store'])->name('create-todo');
-
-
-
 Route::get('/todo', [TodoController::class, 'index'])->name('get-todo');
+
 
 Route::get('/ping', function () {
     return response()->json(['message' => 'Server is Live!'], 200);
 });
 Route::get('/info', [PluginInfoController::class, 'servePluginInfo']);
 Route::get('/sidebar', [SideBarItemsController::class, 'serveMenuItems']);
-Route::delete('delete/{id}', [TaskCommentController::class,'delete']);
+
+
