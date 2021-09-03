a00c13cff3da505ab2903f311e7e966021fd1cb7
trojan782 <davidjacobs522@gmail.com>
2021-09-03 13:59:56 +0100

    deleted the resource controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\TaskService;
 use Illuminate\Http\Request;
+use App\Services\TaskService;
+use App\Http\Resources\TodoResource;
 use Illuminate\Support\Facades\Validator;
 
 class TodoController extends Controller
@@ -74,5 +75,29 @@ class TodoController extends Controller
         }
 
         return response()->json($search, 200);
+    }
+
+    //Function to test the resource
+     public function showResource() : TodoResource
+    {
+        $data =  [
+            'id' => '0',
+            'title' => 'Demo Todo',
+            'description' => 'This is a demo todo to test the resource',
+            'status_id' => '1',
+            'start_date' => '2021-09-03',
+            'end_date' => '2021-09-05',
+            'parent_id' => '012',
+            'workspace_id' => '013',
+            'category_id' => '01',
+            'priority_id' => '03',
+            'recurring' => [
+                'status' => true
+            ],
+            'color_code' => '#001',
+            'created_at' => '2021-09-03',
+            'updated_at' => 'null'
+        ];
+        return new TodoResource($data);
     }
 }
