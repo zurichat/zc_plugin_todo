47b0e5615702ee39ef0db43d0ab21ab3893fadb2
Alpha2Chris14 <54399257+Alpha2Chris14@users.noreply.github.com>
2021-09-03 12:14:37 -0400

    added updated route
    added updated route  /comment/{id}

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,8 @@ Route::get('/comment', function(){
 });
 
 //This Fetch id of task for comment
-Route::get('/alphachris/comment/{id}',[TaskCommentController::class,'findTaskCommentById']);
+Route::get('/comment/{id}',[TaskCommentController::class,'findTaskCommentById']);
+Route::get('/alphachris/comment/{id}',[TaskCommentController::class,'findTaskCommentByIdChris']);
 
 //Resource route
 Route::apiResource('todo_resource', \App\Http\Controllers\API\TodoResourceController::class)->only('index');
