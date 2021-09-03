c414201e66cd962e639e42e3e3a9cb2048327193
Victor Okonkwo <emmyvic98@gmail.com>
2021-09-03 11:14:40 +0100

    Fixed issue #36

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\TodoController;
 use Symfony\Component\VarDumper\VarDumper;
 use App\Http\Controllers\PluginInfoController;
 use App\Http\Controllers\SideBarItemsController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,8 @@ Route::get('task/{id}', [\App\Http\Controllers\TaskDemoController::class, 'show'
 Route::post('task', [\App\Http\Controllers\TaskDemoController::class, 'store']);
 Route::put('task/{id}', [\App\Http\Controllers\TaskDemoController::class, 'update']);
 Route::delete('task/{id}', [\App\Http\Controllers\TaskDemoController::class, 'delete']);
+
+Route::get('task-category',[TaskController::class,'getTasksByCategory']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
