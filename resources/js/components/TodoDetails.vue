b30ab7bfe22f0dccaa0e148fe1d3a21f6654de9b
Scientisco <adeyinkaanees@yahoo.com>
2021-09-03 21:10:53 +0100

    tried the details agin

--- resources/js/components/TodoDetails.vue
+++ resources/js/components/TodoDetails.vue
@@ -1,10 +1,23 @@
 <template>
     <div class="todo-details">
-        <h3 class="title">Task Title</h3>
+        <h3 class="title">{{Title}}</h3>
         <h4 class="descrp">Description</h4>
-        <p><i class="fas fa-calendar"></i>Tomorrow</p>
+        <p><i class="fas fa-calendar-alt"></i>Tomorrow</p>
         <p>Collaborators</p>
         <p class="recurring :">Recurring :</p>
     </div>
     
 </template>
+<script>
+export default{
+    name: "TodoDetails",
+    props:{
+        Title:{
+            type: String,
+            required: true,
+            default: "Task Title",
+        
+        },
+    },
+};
+</script>
--- resources/js/pages/Main.vue
+++ resources/js/pages/Main.vue
@@ -7,13 +7,9 @@
     
 </template>--->
 <template>
-    <div class="todo-details">
-        <h3 class="title">Task Title</h3>
-        <h4 class="descrp">Description</h4>
-        <p><i class="fas fa-calendar"></i>Tomorrow</p>
-        <p>Collaborators</p>
-        <p class="recurring :">Recurring :</p>
-    </div>
+    <div>
+   <TodoDetails />
+   </div>
     
 </template>
 
