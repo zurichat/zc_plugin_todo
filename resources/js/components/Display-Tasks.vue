0d8fcf0d901209506647925f7576d1dec47d73ee
isonguyom <isonguyom@gmail.com>
2021-09-03 16:49:27 +0100

    add display task section

---
+++ resources/js/components/Display-Tasks.vue
@@ -0,0 +1,12 @@
+<template>
+  <section
+    class="display_tasks w-full flex flex-wrap justify-between bg-transparent"
+  ></section>
+</template>
+
+
+<style>
+.display_tasks {
+  height: fit-content;
+}
+</style>
