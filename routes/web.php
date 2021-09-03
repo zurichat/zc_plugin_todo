46ab10684ad506fc461ff5af92d3fe375de5fbc1
trojan782 <davidjacobs522@gmail.com>
2021-09-03 11:06:39 +0100

    added the controller

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PluginInfoController;
 use App\Http\Controllers\SideBarItemsController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\TodoResourceController;
 
 /*
 |--------------------------------------------------------------------------
