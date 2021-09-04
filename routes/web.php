0ea97925362a8e8d4d819c65da6a4c8a4f69de05
Ericogia <ogia.aghahowa@gmail.com>
2021-09-04 10:35:26 +0100

    Deleted route repetitions in routes/web.php file

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PluginInfoController;
 use App\Http\Controllers\SideBarItemsController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,11 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/plugin-info', [PluginInfoController::class, 'servePluginInfo']);
+
 Route::get('/create-todo', [TodoController::class, 'showPage'])->name('show.create-todo');
 Route::post('/create-todo', [TodoController::class, 'store'])->name('create-todo');
 
@@ -28,8 +30,4 @@ Route::post('/create-todo', [TodoController::class, 'store'])->name('create-todo
 
 Route::get('/todo', [TodoController::class, 'index'])->name('get-todo');
 
-Route::get('/ping', function () {
-    return response()->json(['message' => 'Server is Live!'], 200);
-});
 Route::get('/info', [PluginInfoController::class, 'servePluginInfo']);
-Route::get('/sidebar', [SideBarItemsController::class, 'serveMenuItems']);
