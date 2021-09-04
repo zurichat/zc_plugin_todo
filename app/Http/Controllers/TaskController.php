64f508375221d482b4343d1741f84d2fcaf002ba
unknown <alabo2015@yahoo.com>
2021-09-04 02:48:47 +0100

    files

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\TaskService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TaskController extends Controller
+{
+
+    protected $taskService;
+
+    public function __construct(TaskService $taskService)
+    {
+        $this->taskService = $taskService;
+    }
+
+    /**
+     * Show the search results.
+     *
+     * @return mixed
+     */
+    public function search(Request $request)
+    {
+        return response()->json($this->taskService->search($request->query('key'), $request->query('q')));
+    }
+
+    public function getLatestTask()
+    {
+        return response()->json($this->taskService->getLatestTask());
+    }
+
+    public function show($id)
+    {
+        $tasks = ($this->taskService->find($id))['data'];
+        $data = [];
+        foreach($tasks as $task){
+            if($task['_id'] == $id){
+                $data[] = $task;
+            }
+        }
+        return response()->json([
+                "status" => 200,
+                "message" => "success",
+                'data' => $data,
+                    ]);
+    }
+    public function modifyShow($id)
+    {
+        return view('updateDueDate');
+    }
+    public function updateTaskDate(Request $request, $id)
+    {
+        return response()->json($this->taskService->update($request->all(), $id));
+    }
+
+
+    public function getTasksByCategory(Request $request)
+    {
+        // Validation of input
+        $validator = Validator::make($request->all(), [
+            'category_id' => 'required|integer'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'Error'=>'Request failed',
+                'message' => $validator->errors()
+            ], 400);
+        }
+        // Search for the category
+        $allTasks = $this->taskService->all()['data'];
+        $newArr = [];
+        foreach ($allTasks as $value) {
+            if (isset($value['category_id']) && $value['category_id'] == $request->category_id) {
+                array_push($newArr, $value);
+            }
+        }
+        return response()->json([
+            'message' => 'Request success',
+            'data' => $newArr
+        ],200);
+    }
+}
