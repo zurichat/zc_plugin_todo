e79633ae9ec6daf146f72f0f4b34981e1dfe0230
Olayinka Promise <olayinkapromise80@gmail.com>
2021-09-04 20:02:29 +0100

    added task function

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -106,4 +106,22 @@ class TaskController extends Controller
     {
         return response()->json($this->taskService->update($request->all(), $id));
     }
+
+    public function getTaskCollection($id)
+    {
+        $user = [
+            'id' => $id,
+            'name' => 'Olayinka',
+            'tasks' => ['play games', 'go to church', 'wake up by 5']
+        ];
+
+        return response()->json([
+            'status' => 'success',
+            'error' => false,
+            'message' => 'user found',
+            'data' => [
+                'user' => $user
+            ]
+        ], 200);
+    }
 }
