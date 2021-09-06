0504292d30685787b24e2c50d2da94b8b370b2af
trojan782 <davidjacobs522@gmail.com>
2021-09-06 14:01:26 +0100

    modified the resource

--- app/Http/Resources/TodoResource.php
+++ app/Http/Resources/TodoResource.php
@@ -15,21 +15,22 @@ class TodoResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            '_id' => $this->_id,
-            'title' => $this->title,
-            'description' => $this->description,
-            'status_id' => $this->status_id,
-            'start_date' => $this->start_date,
-            'status_id' => $this->status_id,
-            'end_date' => $this->end_date,
-            'parent_id' => $this->parent_id,
-            'workspace_id' => $this->workspace_id,
-            'category_id' => $this->category_id,
-            'priority_id' => $this->priority_id,
-            'reminder' =>$this->reminder,
-            // 'created_at' => $this->created_at,
-            // 'updated_at' => $this->updated_at
-        ];
+        // return [
+        //     '_id' => $this->_id,
+        //     'user_id' => $this->user_id,
+        //     'title' => $this->title,
+        //     'description' => $this->description,
+        //     'start_date' => $this->start_date,
+        //     'end_date' => $this->end_date,
+        //     'status' => [],
+        //     'collaborators' => [],
+        //     'labels' => [],
+        //     'category_id' => $this->category_id,
+        //     'priority_id' => $this->priority_id,
+        //     'reminder' =>$this->reminder,
+        //     // 'created_at' => $this->created_at,
+        //     // 'updated_at' => $this->updated_at
+        // ];
+        return parent::toArray($request);
     }
 }
