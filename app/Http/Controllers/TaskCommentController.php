adf2417d4c6e501f4a82202a7fc95e2dcd40f23e
Ericogia <ogia.aghahowa@gmail.com>
2021-09-06 21:38:41 +0100

    Cleaned up CommentController

--- app/Http/Controllers/TaskCommentController.php
+++ app/Http/Controllers/TaskCommentController.php
@@ -33,7 +33,6 @@ class TaskCommentController extends Controller
 
     public function saveComment(Request $request)
     {
-
         $input = $request->validate([
             'user_id' => 'required',
             'task_id' => 'required',
@@ -44,79 +43,17 @@ class TaskCommentController extends Controller
         return response()->json($this->taskCommentService->create($payload));
     }
 
-    public function store(Request $request)
-    {
-        if (!empty($request->user_id) && !empty($request->task_id)) {
-            return response()->json($this->taskCommentService->create($request->all()));
-        } else {
-            // create random ids for comment
-            $userId = rand(1, 20);
-            $taskId = 1;
-
-            // sketch the response data
-            $data = [
-                "content" => $request->content,
-                "task_id" => $taskId,
-                "user_id" => $userId,
-                "created_at" => Carbon::now(),
-            ];
-
-            // return response
-            return response()->json($this->taskCommentService->create($data));
-        }
-    }
-
-    /**
-     * @author {Alpha2Chris14}
-     */
-    public function findTaskCommentById($id)
-    {
-        return response()->json($this->taskCommentService->findTaskCommentById($id));
-    }
-
-    public function findTaskCommentByIdTest($id)
-    {
-        $url = $this->taskCommentService->all();
-
-        $datas = $url["data"];
-
-        $comments = array();
-        foreach ($datas as $data) {
-            if (array_key_exists('task_id', $data)) {
-                $taskId = $data["task_id"];
-                if ($taskId == $id) {
-                    $comments[] = $data;
-                }
-            }
-        }
-
-        return response()->json($comments);
-    }
-
-    public function show($id)
-    {
-        return response()->json($this->taskCommentService->find($id));
-    }
 
     public function update(Request $request, $id)
     {
         $comment = $this->taskCommentService->find($id);
-        if(!$comment){
-            return response()->json(['message' => 'Comment not found'], 404);    
+        if (!$comment) {
+            return response()->json(['message' => 'Comment not found'], 404);
         }
         return response()->json($this->taskCommentService->update($request->all(), $comment['_id']));
     }
 
-    public function editcomment($id){
-        $comment = $this->taskCommentService->find($id);
-        if(!$comment){
-            return response()->json(['message' => 'Comment not found'], 404);    
-        }
-        return view('editcomment')->with([
-            'id' => $id,
-            'comment' => $comment,
-        ]);
-    }
+
 
     public function delete($id)
     {
