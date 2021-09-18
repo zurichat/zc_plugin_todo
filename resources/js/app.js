aa74686d9d71fa17fd745626daffb04308c308c1
didier-ds <didiersenou94@gmail.com>
2021-09-18 20:31:48 +0100

    single spa

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,43 +2,40 @@ require('./bootstrap');
 import 'primevue/resources/primevue.min.css'; //corecss
 import 'primeicons/primeicons.css';
 import 'primevue/resources/themes/saga-green/theme.css';
-import links from './plugins/links'
 import router from './router';
 import store from './store'
 import axios from "axios";
-
 axios.defaults.baseURL = 'https://todo.zuri.chat/api/v1'
-
-const appendLinks = () => {
-    const head = document.querySelector('head');
-
-    console.log(links)
-    for (let obj of links) {
-        var link = document.createElement('link');
-        link.type = obj.type;
-        link.rel = obj.rel;
-        head.appendChild(link)
-        link.href = obj.href;
-        console.log(link)
-    }
-
-}
 import VueClickAway from "vue3-click-away";
-import { createApp } from 'vue'
-import { Vue3Mq } from "vue3-mq";
 import App from './App.vue'
 const app = createApp(App);
-app.use(Vue3Mq, {
-        breakpoints: {
-            xs: 0,
-            sm: 576,
-            md: 768,
-            lg: 992,
-            xl: 1200,
-            xxl: 1400
+import './set-public-path';
+import { h, createApp } from 'vue';
+import singleSpaVue from 'single-spa-vue';
+const vueLifecycles = singleSpaVue({
+    createApp,
+    appOptions: {
+        render() {
+            return h(App, {
+                // single-spa props are available on the "this" object. Forward them to your component as needed.
+                // https://single-spa.js.org/docs/building-applications#lifecyle-props
+                // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
+                /*
+                name: this.name,
+                mountParcel: this.mountParcel,
+                singleSpa: this.singleSpa,
+                */
+            });
         },
-    }).use(router)
-    .use(VueClickAway)
-    .use(store)
-    .mount("#app")
-appendLinks();
+    },
+    handleInstance(app) {
+        app.use(router);
+        app.use(VueClickAway)
+        app.use(store);
+        app.mount("#app")
+    },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
