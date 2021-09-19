38e3d9973a8acadfa3877b74be41a2d99b7b776d
didier-ds <didiersenou94@gmail.com>
2021-09-19 15:02:49 +0100

    search function

--- frontend/src/components/Progress.vue
+++ frontend/src/components/Progress.vue
@@ -29,14 +29,14 @@
             }
         },
         methods: {
-             progress(){
+             progress(){ 
                  const numb = document.querySelector(".numb");
             let counter = 0;
             setInterval(()=>{
               if(counter == 100){
                 clearInterval();
               }else{
-                counter+=1;
+                counter+=1; 
                 numb.textContent = counter + "%";
               }
             }, 80);
