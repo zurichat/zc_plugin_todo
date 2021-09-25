454e5845cee1ff3fc61bced54cf3915ee1f6035b
Mesh Manuel <meshmmanuel@gmail.com>
2021-09-25 06:48:50 +0100

    change middleware group for vars middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\AssignHTTPRepositoryVarsMiddleware::class,
     ];
 
     /**
@@ -44,6 +43,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\AssignHTTPRepositoryVarsMiddleware::class,
         ],
     ];
 
