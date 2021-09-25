075004b9cd506be05d485df1943168a5ecc787fd
Mesh Manuel <meshmmanuel@gmail.com>
2021-09-25 04:09:10 +0100

    add user_id to vars middleware

--- app/Http/Resources/SidebarResource.php
+++ app/Http/Resources/SidebarResource.php
@@ -18,9 +18,9 @@ class SidebarResource extends JsonResource
         return [
             'name' => 'Todo Plugin',
             'description' => 'Shows todo items',
-            'plugin_id' => Config::get('organisation_id'),
+            'plugin_id' => Config::get('plugin_id'),
             'organisation_id' => Config::get('organisation_id'),
-            'user_id' => Config::get('organisation_id'),
+            'user_id' => Config::get('user_id'),
             'group_name' => 'Todo',
             'sidebar_logo' => 'https://media.publit.io/file/calendar-page-with-circular-clock-symbol-svgrepo-com.svg',
             'joined_rooms' => SidebarRoomsResource::collection($this['joined_rooms']),
