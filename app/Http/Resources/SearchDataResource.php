e69fc59e94f4184f97ea6591204f5cab41b4e14a
kayleb01 <calebbala15@gmail.com>
2021-10-15 19:19:35 +0000

    fix the search

--- app/Http/Resources/SearchDataResource.php
+++ app/Http/Resources/SearchDataResource.php
@@ -15,10 +15,12 @@ class SearchDataResource extends JsonResource
     public function toArray($request)
     {
         return [
+            '_id' => $this['_id'],
             'title' => $this['title'],
             'email' => '',
             'content' => $this['description'],
             'image_url' => [],
+            'created_by' => [],
             'created_at' => $this['created_at'],
             'destination_url' => '/detail/'.$this['_id']
         ];
